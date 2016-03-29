2291cf2313ef872691d9b5c64b8555863f0b97c4
wongxinjie <wongxinjierun@gmail.com>
2016-03-30 02:24:10 +0800

    modify wget.py

--- downloader/wget.py
+++ downloader/wget.py
@@ -19,7 +19,7 @@ import requests
 
 class PyWget(object):
 
-    def __init__(self, filename=None, config={}):
+    def __init__(self, config={}):
         self.config = {
             'block': config.get('block') if 'block' in config else 10240,
         }
@@ -40,20 +40,13 @@ class PyWget(object):
         return name
 
     def support_continue(self, url):
-        headers = {
-            'Range': 'bytes=0-4'
-        }
-        try:
-            r = requests.head(url, headers=headers)
-            crange = r.headers['content-range']
+        r = requests.head(url, allow_redirects=True)
+        crange = r.headers.get('content-range')
+        if crange:
             self.total = int(re.match(ur'^bytes 0-4/(\d+)$', crange).group(1))
             return True
-        except:
-            pass
-        try:
-            self.total = int(r.headers['content-length'])
-        except:
-            self.total = 0
+
+        self.total = int(r.headers.get('Content-Length', 0))
         return False
 
     def download(self, url, filename=None, headers={}):
@@ -65,8 +58,9 @@ class PyWget(object):
         local_filename = self.remove_nonchars(filename)
         tmp_filename = local_filename + '.downtmp'
         size = self.size
-        total = self.total
-        if self.support_continue(url):  # 支持断点续传
+
+        # 支持断点下载
+        if self.support_continue(url):
             try:
                 with open(tmp_filename, 'rb') as fin:
                     self.size = int(fin.read())
@@ -74,12 +68,14 @@ class PyWget(object):
             except:
                 self.touch(tmp_filename)
             finally:
-                headers['Range'] = "bytes=%d-" % (self.size, )
+                headers['Range'] = "bytes=%d-" % self.size
         else:
             self.touch(tmp_filename)
             self.touch(local_filename)
 
-        r = requests.get(url, stream=True, verify=False, headers=headers)
+        total = self.total
+
+        r = requests.get(url, stream=True, headers=headers)
         if total > 0:
             logging.info("[+] Size: %dKB", (total / 1024))
         else:
@@ -95,7 +91,9 @@ class PyWget(object):
                         size += len(chunk)
                         f.flush()
                     logging.info(
-                        '\b' * 64 + 'now: %d, total: %s' % (size, total))
+                        'now: {0:.2f}KB, total: {1:.2f}kB, {2:.2f}%'.format(
+                            size / 1024.0, total / 1024.0, size * 100.0 / total
+                        ))
                     sys.stdout.flush()
                 finished = True
                 os.remove(tmp_filename)
@@ -103,7 +101,7 @@ class PyWget(object):
                 speed = int((size - self.size) / 1024 / spend)
                 sys.stdout.write(
                     ('\nDownload finished!\nTotal time: %ss,'
-                     ' Download speed: %sk/s\n') % (spend, speed))
+                     ' Download speed: %sKB/s\n') % (spend, speed))
                 sys.stdout.flush()
             except Exception as err:
                 track = traceback.format_exc()
@@ -115,9 +113,14 @@ class PyWget(object):
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG,
+                        format="%(asctime)s %(levelname)s# %(message)s",
+                        datefmt="%Y/%m/%d-%H:%M:%S")
     args = sys.argv
     if len(args) < 2:
-        print 'python2 wget.py url'
+        print 'python2 wget.py filename url'
         sys.exit(1)
-    url = args[1]
-    PyWget().download(url)
+    filename = args[1]
+    url = args[2]
+    wget = PyWget()
+    wget.download(url, filename)
